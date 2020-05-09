eea5214b6a5baeed4e0e4bd5fbe75a7a150a7cb9
Michael Chirico <michael.chirico@grabtaxi.com>
2020-05-09 12:50:26 +0800

    more field extractions, some extra helpers

--- backfill.R
+++ backfill.R
@@ -16,10 +16,38 @@ for (bug_i in seq_len(nrow(head(bugDF, MAX_BUGS_TO_READ)))) {
 
   bug_page = jump_to(session, BUG_URL)
 
+  # column of metadata on the LHS; filtering down first to this sub-node
+  #   speeds up the repeated queries of its children substantially [~4x]
+  meta_table = bug_page %>% html_node(xpath = '//td[@id="bz_show_bug_column_1"]')
+  time_table = bug_page %>% html_node(xpath = '//td[@id="bz_show_bug_column_2"]')
+  attachments = bug_page %>% html_node(xpath = '//table[@id="attachment_table"]') %>%
+    html_nodes(xpath = './/tr[number(substring-after(@id, "a")) > 0]')
+
   bug = list(
-    summary = bug_page %>% html_node('span#short_desc_nonedit_display') %>% html_text_clean,
-    status = bug_page %>% html_node('span#static_bug_status') %>% html_text_clean,
-    alias = bug_page %>% html_node(xpath = '//tr[@id="field_tablerow_alias"]/td') %>% html_text_clean
+    summary = get_field(bug_page, 'span', 'short_desc_nonedit_display'),
+
+    status = get_field(meta_table, 'span', 'static_bug_status'),
+    alias = get_field(meta_table, 'tr/td', 'field_tablerow_alias'),
+    component = get_field(meta_table, 'td', 'field_container_component', clean = FALSE) %>%
+      gsub('(show other bugs)', '', . , fixed = TRUE) %>% clean,
+    version = get_field(meta_table, 'tr/td', 'field_tablerow_version'),
+    hardware = get_field(meta_table, 'tr/td', 'field_tablerow_rep_platform'),
+    importance = get_field(meta_table, 'tr/td', 'field_tablerow_importance'),
+    assignee = get_field(meta_table, 'tr/td', 'field_tablerow_assigned_to'),
+    url = get_field(meta_table, 'span', 'bz_url_input_area'),
+    depends_on = meta_table %>%
+      html_nodes(xpath = './/tr[@id="field_tablerow_dependson"]/td/a') %>%
+      html_text_clean %>% as.integer,
+    blocks = meta_table %>%
+      html_nodes(xpath = './/tr/th[@id="field_label_blocked"]/parent::node()/td/a') %>%
+      html_text_clean %>% as.integer,
+
+    reported_info = get_field(time_table, 'tr/td', 'field_tablerow_reported'),
+    modified_info = get_field(time_table, 'tr/td', 'field_tablerow_modified', clean = FALSE) %>%
+      gsub('(History)', '', ., fixed = TRUE) %>% clean,
+
+    attachment_info = lapply(attachments, function(a) {
+    })
   )
 
 }
--- utils.R
+++ utils.R
@@ -1,4 +1,5 @@
 library(rvest)
+# begin a session in bugzilla & log in
 bugzilla_session = function(URL = 'https://bugs.r-project.org') {
   session = html_session(URL)
 
@@ -18,10 +19,31 @@ bugzilla_session = function(URL = 'https://bugs.r-project.org') {
   return(session)
 }
 
-html_text_clean = function(x, replace_newline = TRUE, replace_nbsp = TRUE, collapse_ws = TRUE) {
-  text = trimws(html_text(x))
+# strip several kinds of whitespace
+clean = function(x, replace_newline = TRUE, replace_nbsp = TRUE, collapse_ws = TRUE) {
+  text = trimws(x)
   if (replace_newline) text = gsub('\n', ' ', text, fixed = TRUE)
   if (replace_nbsp) text = gsub(intToUtf8(160L), ' ', text, fixed = TRUE)
   if (collapse_ws) text = gsub('\\s{2,}', ' ', text)
   return(text)
+}
+
+# wrapper for html_text %>% clean
+html_text_clean = function(x, replace_newline = TRUE, replace_nbsp = TRUE, collapse_ws = TRUE) {
+  return(clean(html_text(x), replace_newline, replace_nbsp, collapse_ws))
+}
+
+# common extractions:
+#   <tr id='...'><td>desired text</td></tr>
+#   <span|td id='...'>desired test</span|td>
+# .// (not //) to be sure we are only search "below" page on the tree
+get_field = function(page, type, id, clean = TRUE, ...) {
+  out = switch(type,
+    span = , td =
+      html_node(page, xpath = sprintf('.//%s[@id="%s"]', type, id)),
+    'tr/td' =
+      html_node(page, xpath = sprintf('.//tr[@id="%s"]/td', id)),
+    stop("Unrecognized 'type'")
+  )
+  if (clean) html_text_clean(out, ...) else html_text(out)
 }
